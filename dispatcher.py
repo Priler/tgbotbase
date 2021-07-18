424f55150ceba372159c4f773bd1eabb371be90f
Abraham Tugalov <priler96@gmail.com>
2021-07-18 17:34:09 +0500

    New filters bind, etc

--- dispatcher.py
+++ dispatcher.py
@@ -1,6 +1,14 @@
+import logging
 from aiogram import Bot, Dispatcher
-from filters import IsOwnerFilter
+from filters import IsOwnerFilter, IsAdminFilter, MemberCanRestrictFilter
 import config
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# prerequisites
+if not config.BOT_TOKEN:
+    exit("No token provided")
 
 # init
 bot = Bot(token=config.BOT_TOKEN, parse_mode="HTML")
@@ -8,3 +16,5 @@ dp = Dispatcher(bot)
 
 # activate filters
 dp.filters_factory.bind(IsOwnerFilter)
+dp.filters_factory.bind(IsAdminFilter)
+dp.filters_factory.bind(MemberCanRestrictFilter)
